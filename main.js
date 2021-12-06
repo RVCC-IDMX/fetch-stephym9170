6aab5e55342826767a7aeffa72d8d751d4cf1f38
Your Name <stephym917@gmail.com>
2021-12-05 19:40:41 -0500

    Finished

--- main.js
+++ main.js
@@ -1,1 +1,58 @@
-/* fetch.js */
+//fetch('demo.json')
+//.then(res => { console.log(res);
+    //return res.json();
+//})
+//.then(json => console.log(json))
+
+fetch('https://unsplash.it/600/400')
+.then(res = res.blob())
+.then(blob => {
+    let img = document.createElement('img');
+    img.src = URL.createObjectURL(blob);
+    document.querySelector('body').appendChild(img);
+});
+
+
+
+const postSection = document.querySelector('#posts');
+const postTemplate = document.querySelector('#post-template');
+
+getData()
+.catch(err => console.log(err));
+
+async function getData() {
+    const postStream = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await postStream.json();
+    let i = 0;
+
+    const img = await fetch('https://unsplash.it/300/200')
+    //console.log(posts)
+
+    posts.forEach(post => {
+        i++;
+        if(i < 10) {
+        const title = post.title;
+        const body = post.body;
+
+        fetch('https:unsplash.it/300/200')
+        .then(res => res/blob())
+        .then(blob => {
+            const newPost = document.importNode(postTemplate.content, true);
+            const postTitle = newPost.querySelector
+            ('.post_title');
+            const postBody = newPost.querySelector
+            ('.post_body');
+            const postImg = newPost.querySelector
+            ('.post_img');
+
+            postImg.src = URL.createObjectURL(blob);
+            postTitle.innerText = title;
+            postBody.innterText = body;
+            postSection.appendChild(newPost);
+        })
+        .catch(err => console.log(err));
+
+        
+        }
+    })
+}
